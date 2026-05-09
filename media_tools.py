5b8f8175a762e3361bcd4700bf8ee2cb0b3e3ce5
pallavi-m-2005 <166612835+pallavi-m-2005@users.noreply.github.com>
2026-05-09 11:08:41 +0530

    Create media_tools.py

---
+++ media_tools.py
@@ -0,0 +1,25 @@
+from moviepy.editor import VideoFileClip
+from pydub import AudioSegment
+
+
+def compress_video(input_path, output_path):
+
+    video = VideoFileClip(input_path)
+
+    video.write_videofile(
+        output_path,
+        bitrate="500k"
+    )
+
+    return "Video Compressed Successfully"
+
+
+def trim_audio(input_path, output_path, start_ms, end_ms):
+
+    audio = AudioSegment.from_file(input_path)
+
+    trimmed_audio = audio[start_ms:end_ms]
+
+    trimmed_audio.export(output_path, format="mp3")
+
+    return "Audio Trimmed Successfully"
